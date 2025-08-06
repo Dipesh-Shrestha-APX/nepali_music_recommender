46bd72849a96cc33ab341d0dc30d44d8cb225ad9
Dipesh <deepeshpixar123@gmail.com>
2025-08-06 20:41:41 +0545

    Trimmed a bit

--- api/main.py
+++ api/main.py
@@ -1,38 +1,3 @@
-# import sys
-# import os
-
-# # Add current directory to path
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-# from fastapi import FastAPI, HTTPException, Query
-# from recommend_content import recommend
-
-# app = FastAPI()
-
-# MODEL_PATH = "app/model/als_model.pkl"
-# TRACKS_PATH = "app/data/tracks.csv"
-# INTERACTIONS_PATH = "app/data/interactions.csv"
-
-
-# @app.get("/")
-# def root():
-#     return {"message": "Recommender System running."}
-
-# @app.get("/recommend_content/")
-# def get_recommendations(song_title: str = Query(..., alias="title"), top_n: int = 10):
-#     results = recommend(song_title, top_n)
-#     if not results:
-#         raise HTTPException(status_code=404, detail="Song not found")
-#     return {"input": song_title, "recommendations": results}
-
-# @app.get("/recommend_collab/")
-# def get_recommendations(song_title: str = Query(..., alias="title"), top_n: int = 10):
-#     results = recommend(song_title, top_n)
-#     if not results:
-#         raise HTTPException(status_code=404, detail="Song not found")
-#     return {"input": song_title, "recommendations": results}
-# main.py
-
 import sys
 import os
 
--- api/recommend_content.py
+++ api/recommend_content.py
@@ -1,32 +1,3 @@
-# import pickle
-# import pandas as pd
-
-# import os
-
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# with open(os.path.join(BASE_DIR, "content_model", "tfidf_vectorizer.pkl"), "rb") as f:
-#     tfidf = pickle.load(f)
-
-# with open(os.path.join(BASE_DIR, "content_model", "tfidf_matrix.pkl"), "rb") as f:
-#     tfidf_matrix = pickle.load(f)
-
-# with open(os.path.join(BASE_DIR, "content_model", "similarity_matrix.pkl"), "rb") as f:
-#     similarity_df = pickle.load(f)
-
-# songs = pd.read_csv(os.path.join(BASE_DIR, "content_model", "songs_with_features.csv"))
-
-# # Recommendation function
-# def recommend(song_reference, top_n=10):
-#     if song_reference not in similarity_df.index:
-#         return []
-#     # Get similar songs
-#     song_index = similarity_df.index.get_loc(song_reference)
-#     top = similarity_df.iloc[song_index].sort_values(ascending=False)[1:top_n+1]
-#     return [{"title": title, "score": float(score)} for title, score in top.items()]
-
-
-
 import os
 import pickle
 import pandas as pd
